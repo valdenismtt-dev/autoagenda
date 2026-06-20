1ff487c6733e1a85c88bb672629afc69c05ad45b
valdenismtt-dev <valdenismtt-dev@users.noreply.github.com>
2026-06-20 16:44:45 -0300

    first commit

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE = 'autoagenda-v1';
+const ASSETS = [
+  'autoagenda.html',
+  'manifest.json',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
+  'https://fonts.gstatic.com/s/inter/v13/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa25L7W0Q5n-wU.woff2',
+  'https://unpkg.com/feather-icons@4.29.0/dist/feather.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.origin === location.origin || url.hostname === 'fonts.googleapis.com' || url.hostname === 'fonts.gstatic.com' || url.hostname === 'unpkg.com') {
+    event.respondWith(
+      caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+        const clone = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, clone));
+        return response;
+      }).catch(() => caches.match('autoagenda.html')))
+    );
+  }
+});
